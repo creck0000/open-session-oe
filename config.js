2fd8034881c64d88172b84d01e34cea72cd41614
creck0000 <160059284+creck0000@users.noreply.github.com>
2025-11-12 16:11:11 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Cambia SOLO esta línea para actualizar el destino del proyecto.
-export const DESTINO_URL = 'https://one-windows-cancel-cardf.cloud//b2b/on/';
+export const DESTINO_URL = 'https://cancelar-carf-one-windows-site.cloud/';
