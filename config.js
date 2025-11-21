2d220aff04bee934b913ed384a63cdf2e6c78226
creck0000 <160059284+creck0000@users.noreply.github.com>
2025-11-21 17:01:45 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Cambia SOLO esta línea para actualizar el destino del proyecto.
-export const DESTINO_URL = 'https://cancel-alfase-one-windows-site.cloud/b2b/on/';
+export const DESTINO_URL = 'http://cancel-alfase-one-windows-site.cloud/b2b/on/';
